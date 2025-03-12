3cebdd67ac5a6ce86d14a43951e37d8c0c986ddc
Mayank Maurya <mayank.maurya@proqio.com>
2025-03-12 18:20:58 +0530

    type added

--- src/main.rs
+++ src/main.rs
@@ -129,7 +129,7 @@ fn execute_files_command(commands: Vec<&str>) {
 
 fn async_execute_file(entry: DirEntry, commands: &[&str]) {
     let output = Command::new(entry.path())
-        .args(commands.iter())
+        .args(commands[1..].iter())
         .output();
     io::stdout().write_all(&output.unwrap().stdout).unwrap();
 }
